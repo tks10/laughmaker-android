2bc7de3e955f3b16c3cc3f3dc36d87e7dbf1fb72
tks10 <tktktks10@gmail.com>
2018-12-27 01:25:13 +0900

    feat: TakeMovieFragment

---
+++ app/src/main/java/com/takashi/laughmaker/takemovie/TakeMovieFragment.kt
@@ -0,0 +1,22 @@
+package com.takashi.laughmaker.takemovie
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TakeMovieFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
