1d48f1691c3c03606cc14dff4bf951797ae2a1f2
tks10 <tktktks10@gmail.com>
2018-12-27 11:12:14 +0900

    feat: preview fragment

---
+++ app/src/main/java/com/takashi/laughmaker/preview/PreviewFragment.kt
@@ -0,0 +1,24 @@
+package com.takashi.laughmaker.preview
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class PreviewFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+
+        return view
+    }
+}
