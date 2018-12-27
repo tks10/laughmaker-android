8a3ddab877f640f8c4452cd13978e68652955566
tks10 <tktktks10@gmail.com>
2018-12-27 21:22:07 +0900

    feat: Result List Adapter

--- app/src/main/java/com/takashi/laughmaker/result/ResultListAdapter.kt
+++ app/src/main/java/com/takashi/laughmaker/result/ResultListAdapter.kt
@@ -1,2 +1,28 @@
 package com.takashi.laughmaker.result
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.takashi.laughmaker.databinding.ResultItemBinding
+import com.takashi.laughmaker.result.Entity.*
+
+class ResultListAdapter(val context: Context, private val resultImages: List<ResultImage>)
+    : RecyclerView.Adapter<ResultListAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ResultItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ResultItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val resultImage = resultImages[position]
+        // Binding
+        holder.binding.resultImage = resultImage
+    }
+
+    override fun getItemCount() = resultImages.size
+}
