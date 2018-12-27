5efff823eb9ad96a53c25205465b46e3ad5c2094
tks10 <tktktks10@gmail.com>
2018-12-27 20:45:29 +0900

    feat: bind iitem

--- app/src/main/java/com/takashi/laughmaker/extension/ImageViewExtensions.kt
+++ app/src/main/java/com/takashi/laughmaker/extension/ImageViewExtensions.kt
@@ -4,7 +4,7 @@ import android.databinding.BindingAdapter
 import android.graphics.Bitmap
 import android.widget.ImageView
 
-@BindingAdapter("android:bitmap")
-fun ImageView.bitmap(bitmap: Bitmap) {
+@BindingAdapter("android:bitmap_image")
+fun ImageView.bitmap_image(bitmap: Bitmap) {
     this.setImageBitmap(bitmap)
 }
