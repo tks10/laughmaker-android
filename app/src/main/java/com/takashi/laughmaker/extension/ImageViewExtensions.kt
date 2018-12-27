a45168b8905153829e7a6168c4ef210151caff5b
tks10 <tktktks10@gmail.com>
2018-12-27 21:19:04 +0900

    clean

--- app/src/main/java/com/takashi/laughmaker/extension/ImageViewExtensions.kt
+++ app/src/main/java/com/takashi/laughmaker/extension/ImageViewExtensions.kt
@@ -4,7 +4,7 @@ import android.databinding.BindingAdapter
 import android.graphics.Bitmap
 import android.widget.ImageView
 
-@BindingAdapter("android:bitmap_image")
-fun ImageView.bitmap_image(bitmap: Bitmap) {
+@BindingAdapter("bitmapImage")
+fun ImageView.bitmapImage(bitmap: Bitmap) {
     this.setImageBitmap(bitmap)
 }
