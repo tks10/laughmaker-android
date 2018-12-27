ce2ae7ec1495a7a9237ea25b08f2bfaff7f194f2
tks10 <tktktks10@gmail.com>
2018-12-27 11:09:24 +0900

    feat: catch results

--- app/src/main/java/com/takashi/laughmaker/takemovie/TakeMovieFragment.kt
+++ app/src/main/java/com/takashi/laughmaker/takemovie/TakeMovieFragment.kt
@@ -1,22 +1,41 @@
 package com.takashi.laughmaker.takemovie
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 class TakeMovieFragment : Fragment() {
+    val REQUEST_VIDEO_CAPTURE = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        this.dispatchTakeVideoIntent()
+
+        return view
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun dispatchTakeVideoIntent() {
+        if (activity == null) return
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+            takeVideoIntent.resolveActivity(activity!!.packageManager)?.also {
+                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            val videoUri: Uri = intent.data ?: return
+        }
     }
 }
